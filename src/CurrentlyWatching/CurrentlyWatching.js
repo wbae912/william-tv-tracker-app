783e5b7af9c4ae1eb88309dfd98f5f718bffd53d
William Bae <wbae912@gmail.com>
2019-10-14 17:44:07 -0700

    Hooked up server GET request to Currently Watching and Completed components

--- src/CurrentlyWatching/CurrentlyWatching.js
+++ src/CurrentlyWatching/CurrentlyWatching.js
@@ -1,6 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import TvShowEntry from '../TvShowEntry/TvShowEntry';
 
 export default class CurrentlyWatching extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       watchingShows: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:8000/api/shows/watching`)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong')
+        }
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          watchingShows: data
+        })
+      })
+      .catch(error => {
+        alert(`Error: ${error.message}`)
+      })
+  }
+  
   render() {
     return (
       <div>
@@ -22,31 +49,17 @@ export default class CurrentlyWatching extends Component {
               <option value="1">Rating</option>
           </select>
         </header>
+
+        {this.state.watchingShows.map(show => 
+          <TvShowEntry
+            key={show.id}
+            show={show}
+          />)}
+
         <section>
-          <h2>Stranger Things (Expanded View)</h2>
-          <ul>
-            <p>Season No. 3</p>
-            <p>Episode No. 7</p>
-            <p>Genre: Action/Thriller</p>
-            <p>Rating: 5 stars</p>
-            <p>Description: A group of ordinary kids have their lives turned upside down once they encounter the supernatural...</p>
-            <p>Review: This is one of my favorite shows! It's super suspenseful and has a great story.</p>
-          </ul>
-          <button>Edit</button>
-          <button>Delete</button>
-      </section>
-      <section>
-        <h2>Keeping Up With the Kardashians (Collapsed View)</h2>
-        <p>Season No. 1</p>
-        <p>Episode No. 3</p>
-        <p>Rating: 2 stars</p>
-        <button>Edit</button>
-        <button>Delete</button>
-      </section>
-      <section>
-        <button>Add New TV Show</button>
-      </section>
-    </div>
+          <button>Add New TV Show</button>
+        </section>
+      </div>
     )
   }
 }
